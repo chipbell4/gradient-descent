004ae79fc25a3d6fe34134922c3a0369ccc42d06
Chip Bell <cbell@pbs.org>
2016-06-17 15:57:11 -0400

    Adding a closest approach function, but it's fairly simple now

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
+var V = require('./vector-math');
 var derivative = require('./derivative');
 
 module.exports = function(curve1, curve2) {
+  // create a function that obtains the distance at any provided t1 and t2
+  var distance = {
+    get: (t1, t2) => {
+      var p1 = curve1.get(t1);
+      var p2 = curve2.get(t2);
+      var direction = V.subtract(p1, p2);
+      return V.dot(direction, direction);
+    }
+  }
 
+  // pick an initial starting point
+  var best = { t1: 0, t2: 0, d: 1 / 0 };
+  for(var t1 = 0; t1 <= 1.0; t1 += 0.1) {
+    for(var t2 = 0; t2 <= 1.0; t2 += 0.1) {
+      var newDistance = distance.get(t1, t2);
+      if(newDistance < best.d) {
+        best.t1 = t1;
+        best.t2 = t2;
+        best.d = newDistance;
+      }
+    }
+  }
+
+  return best;
 };
--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 var assert = require('assert');
 var derivative = require('./derivative');
+var closestApproach = require('./index');
 
 var closeTo = (expected, actual, message) => {
   assert(Math.abs(expected - actual) < 0.001, message || 'expected ' + expected + ', got ' + actual);
@@ -25,3 +26,20 @@ var parabola = { get: t => ({ x : t*t, y: t*t }) };
 derivativeTest(parabola, 0, { x: 0, y: 0 });
 derivativeTest(parabola, 1, { x: 2, y: 2 });
 derivativeTest(parabola, 2, { x: 4, y: 4 });
+
+var approachTest = (F1, F2, expectedT1, expectedT2) => {
+  var result = closestApproach(F1, F2);
+  closeTo(result.t1, expectedT1, 'For t1, expected ' + expectedT1 + ' got ' + result.t1);
+  closeTo(result.t2, expectedT2, 'For t2, expected ' + expectedT2 + ' got ' + result.t2);
+};
+var F1, F2;
+
+// two lines that intersect
+F1 = { get: t => ({ x: t, y : 0 }) }
+F2 = { get: t => ({ x: 0, y : 1 - t }) }
+approachTest(F1, F2, 0, 1);
+
+// two parabolas that are nearby (a frown and a smile)
+F1 = { get: t => ({ x: t, y : t * (1 - t) }) };
+F2 = { get: t => ({ x: t, y : 3 - t * (1 - t) }) };
+approachTest(F1, F2, 0.5, 0.5);
