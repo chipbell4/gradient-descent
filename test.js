a5ddf6b6f3df7f23f6e3e9b7953447061614ae44
Chip Bell <cbell@pbs.org>
2016-06-17 15:35:01 -0400

    Quick rename

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ var vectorsClose = (expected, actual) => {
   closeTo(expected.y, actual.y);
 };
 
-var doTest = (F, t, expected) => {
+var derivativeTest = (F, t, expected) => {
   var dF = derivative(F);
   var actual = dF.get(t);
   vectorsClose(expected, actual);
@@ -15,11 +15,11 @@ var doTest = (F, t, expected) => {
 
 // basic line
 var line = { get: t => ({ x: t, y: t }) };
-doTest(line, 0, { x: 1, y: 1});
-doTest(line, 1, { x: 1, y: 1});
+derivativeTest(line, 0, { x: 1, y: 1});
+derivativeTest(line, 1, { x: 1, y: 1});
 
 // parabola
 var parabola = { get: t => ({ x : t*t, y: t*t }) };
-doTest(parabola, 0, { x: 0, y: 0 });
-doTest(parabola, 1, { x: 2, y: 2 });
-doTest(parabola, 2, { x: 4, y: 4 });
+derivativeTest(parabola, 0, { x: 0, y: 0 });
+derivativeTest(parabola, 1, { x: 2, y: 2 });
+derivativeTest(parabola, 2, { x: 4, y: 4 });
