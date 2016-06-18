ade0a3814f33884181539a52ef0facfbebb56df4
Chip Bell <cbell@pbs.org>
2016-06-18 16:43:41 -0400

    Fixing the array based switch

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 var assert = require('assert');
+var V = require('./vector-math');
 var derivative = require('./derivative');
 var closestApproach = require('./index');
 
@@ -6,8 +7,8 @@ var closeTo = (expected, actual, message) => {
   assert(Math.abs(expected - actual) < 0.001, message || 'expected ' + expected + ', got ' + actual);
 }
 var vectorsClose = (expected, actual) => {
-  closeTo(expected.x, actual.x);
-  closeTo(expected.y, actual.y);
+  var delta = V.subtract(actual, expected);
+  closeTo(V.dot(delta, delta), 0);
 };
 
 var derivativeTest = (F, t, expected) => {
@@ -17,15 +18,15 @@ var derivativeTest = (F, t, expected) => {
 };
 
 // basic line
-var line = t => ({ x: t, y: t });
-derivativeTest(line, 0, { x: 1, y: 1});
-derivativeTest(line, 1, { x: 1, y: 1});
+var line = t => [t, t];
+derivativeTest(line, 0, [1, 1]);
+derivativeTest(line, 1, [1, 1]);
 
 // parabola
-var parabola = t => ({ x : t*t, y: t*t });
-derivativeTest(parabola, 0, { x: 0, y: 0 });
-derivativeTest(parabola, 1, { x: 2, y: 2 });
-derivativeTest(parabola, 2, { x: 4, y: 4 });
+var parabola = t => [t*t, t*t];
+derivativeTest(parabola, 0, [0, 0]);
+derivativeTest(parabola, 1, [2, 2]);
+derivativeTest(parabola, 2, [4, 4]);
 
 var approachTest = (F1, F2, expectedT1, expectedT2) => {
   var result = closestApproach(F1, F2);
@@ -34,14 +35,12 @@ var approachTest = (F1, F2, expectedT1, expectedT2) => {
 };
 var F1, F2;
 
-
-
 // two lines that intersect
-F1 = t => ({ x: t, y : 0 })
-F2 = t => ({ x: 0, y : 1 - t })
+F1 = t => [t, 0]
+F2 = t => [0, 1 - t]
 approachTest(F1, F2, 0, 1);
 
 // two parabolas that are nearby (a frown and a smile)
-F1 = t => ({ x: t, y : t * (1 - t) });
-F2 = t => ({ x: t, y : 3 - t * (1 - t) });
+F1 = t => [t, t * (1 - t)];
+F2 = t => [t, 3 - t * (1 - t)];
 approachTest(F1, F2, 0.5, 0.5);
