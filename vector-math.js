7124ddc531aef7624de5c190b07a066e7ae756eb
Chip Bell <cbell@pbs.org>
2016-06-18 16:39:54 -0400

    Converting vector math to work with arrays

--- vector-math.js
+++ vector-math.js
@@ -1,10 +1,18 @@
+var zip = (A, B) => {
+  var zipped = [];
+  for(var i = 0; i < A.length; i++) {
+    zipped.push([ A[i], B[i] ]);
+  }
+  return zipped
+}
+
 var arraySum = A => {
   return A.reduce((sum, value) => sum + value, 0);
 };
 
 // Adds vectors
 module.exports.add = function(P1, P2) {
-  return P1.map((value, index) => value + P2[index]);
+  return zip(P1, P2).map(value => value[0] + value[1]);
 };
 
 // scales vectors
@@ -19,6 +27,6 @@ module.exports.subtract = function(P1, P2) {
 
 // dot product
 module.exports.dot = function(P1, P2) {
-  return arraySum(P1.map((value, index) => value * P2[index]))
+  return arraySum(zip(P1, P2).map(value => value[0] * value[1]));
 };
 
