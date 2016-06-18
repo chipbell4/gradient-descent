3633f804dbf4c3d6ada9205ea2fc69cebb0d819a
Chip Bell <cbell@pbs.org>
2016-06-18 16:36:15 -0400

    First stab at array-based vectors

--- vector-math.js
+++ vector-math.js
@@ -1,11 +1,15 @@
+var arraySum = A => {
+  return A.reduce((sum, value) => sum + value, 0);
+};
+
 // Adds vectors
 module.exports.add = function(P1, P2) {
-  return { x: P1.x + P2.x, y: P1.y + P2.y };
+  return P1.map((value, index) => value + P2[index]);
 };
 
 // scales vectors
 module.exports.scale = function(P1, s) {
-  return { x: P1.x * s, y: P1.y * s };
+  return P1.map(value => value * s);
 };
 
 // subtracts vectors
@@ -15,6 +19,6 @@ module.exports.subtract = function(P1, P2) {
 
 // dot product
 module.exports.dot = function(P1, P2) {
-  return P1.x * P2.x + P1.y * P2.y;
+  return arraySum(P1.map((value, index) => value * P2[index]))
 };
 
