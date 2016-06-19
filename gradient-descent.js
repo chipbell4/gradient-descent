fde0d29993aad378f5265f2cc5dc0cba5d942279
Chip Bell <cbell@pbs.org>
2016-06-19 11:05:20 -0400

    Better precision checking

--- gradient-descent.js
+++ gradient-descent.js
@@ -6,11 +6,18 @@ module.exports = function(F, options) {
   options.gamma = options.gamma || 0.1;
   options.initialValue = options.initialValue || [0, 0];
   options.maxSteps = options.maxSteps || 100;
+  options.precision = options.precision || 0.001;
 
   var gradF = gradient(F);
+  var lastValue = [1 / 0, 1 / 0];
   var currentValue = options.initialValue.map(x => x);
+  var hasConverged = () => {
+    var delta = V.subtract(currentValue, lastValue);
+    return V.dot(delta, delta) < options.precision;
+  };
 
-  for(var i = 0; i < options.maxSteps; i++) {
+  for(var i = 0; i < options.maxSteps && !hasConverged(); i++) {
+    lastValue = currentValue.map(x => x);
     currentValue = V.subtract(currentValue, V.scale(gradF(currentValue), options.gamma));
   }
 
--- test/gradient-descent.js
+++ test/gradient-descent.js
@@ -4,8 +4,17 @@ var gradientDescent = require('../gradient-descent');
 describe('gradient-descent', function() {
   it('should converge to a local minimum of a quadratic', function() {
     var F = vector => Math.pow(vector[0] - 0.5, 2) + Math.pow(vector[1] - 0.7, 2);
-    var result = gradientDescent(F, { maxSteps: 1000, gamma: 0.01, initialValue: [0, 0] });
+    var result = gradientDescent(F, { maxSteps: 1000, gamma: 0.01, initialValue: [0, 0], precision: (0.00001 * 0.00001) });
 
+    expect(result.length).to.equal(2);
+    expect(result[0]).to.be.closeTo(0.5, 0.001);
+    expect(result[1]).to.be.closeTo(0.7, 0.001);
+  });
+
+  it('should stop if its already close', function() {
+    var F = vector => Math.pow(vector[0] - 0.5, 2) + Math.pow(vector[1] - 0.7, 2);
+    var result = gradientDescent(F, { maxSteps: 1000000, gamma: 0.01, initialValue: [0.5, 0.7] });
+    
     expect(result.length).to.equal(2);
     expect(result[0]).to.be.closeTo(0.5, 0.001);
     expect(result[1]).to.be.closeTo(0.7, 0.001);
